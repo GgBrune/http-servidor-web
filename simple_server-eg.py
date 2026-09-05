6a3c1cc5fdc910f1d4859132748196a0008b3ed7
GgBrune <brunellagagliardo@gmail.com>
2026-09-05 13:08:39 -0300

    Ejemplo de servidor simple

--- simple_server-eg.py
+++ simple_server-eg.py
@@ -1,16 +1,22 @@
+# importa de la librería la función 'make_server'
 from wsgiref.simple_server import make_server # Servidor HTTP incluído en py
 
-    # environ = diccionario con la info de la request
+    # environ (embiente del pedido) = diccionario con la info de la request
     # start_response = llamada a función con la respuesta
 def app(environ, start_response):
-    status = "200 OK" # Código de estado a retornar
-    headers = [("Content-Type", "text/plain")] # Headers de la respuesta
+    status = "200 OK" # código de estado a retornar
+    
+    # 'headers' es una colección de elementos - tipo tupla (inumtable)
+    headers = [("Content-Type", "text/plain")] # headers de la respuesta
+    
+    # toma una función como parámetro y la utiliza:
     start_response(status, headers) # Confirmación de status y headers ante el servidor
 
             # 'b' por bytes
-    return [b"Hola"] # Cuerpo de la respuesta
+    return [b"Hola"] # cuerpo de la respuesta
 
-    # "app" función que maneja cada request 
-with make_server("", 9292, app) as server: # Crea el servidor en el puerto '9292'
+    # "app" función que responde cada request 
+with make_server("", 9292, app) as server: # guarda el servidor en la variable 'server'
+# "with" = manejo seguro de arcivhos    
     print("Listening on http://localhost:9292")
-    server.serve_forever() # Deja el servidor corriendo indefinidamente
+    server.serve_forever() # deja el servidor corriendo indefinidamente
